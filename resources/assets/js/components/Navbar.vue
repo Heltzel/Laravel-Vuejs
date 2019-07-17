2630507342b37979476ff96f00edd5898c7e0661
Heltzel <heltzel.service@hotmail.com>
2019-07-17 17:02:33 +0200

    Initial commit

---
+++ resources/assets/js/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-primary mb-2">
+      <div class="container">
+        <a href="#" class="navbar-brand">Larticles</a>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "navbar"
+  }
+</script>
+
+<style scoped>
+
+</style>
